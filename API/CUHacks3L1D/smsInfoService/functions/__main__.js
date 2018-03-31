26254916c61777cf9eb8938347c6018eca4bcc79
Wesley Ma <wesleykhma@gmail.com>
2018-03-31 16:12:56 -0400

    Auto stash before merge of "master" and "origin/master"

--- API/CUHacks3L1D/smsInfoService/functions/__main__.js
+++ API/CUHacks3L1D/smsInfoService/functions/__main__.js
@@ -1,10 +1,17 @@
+const lib = require('lib');
+const sms = lib.utils.sms['@1.0.4'];
+
 /**
 * A basic Hello World function
-* @param {string} name Who you're saying hello to
-* @returns {string}
+* @param {integer} number Who you're notifying
+* @param {string} msg Message to send
+* @returns {object} Message sent
 */
-module.exports = (name = 'world', context, callback) => {
+module.exports = (number, msg = "EMPTY MESSAGE", callback) => {
 
-  callback(null, `hello ${name}`);
-
+  let ret = sms({
+    to: 4169488077, // (required)
+    body: "tett", // (required)
+  }, (err)=>{});
+  return callback(null, ret);
 };
