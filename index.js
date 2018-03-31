da388da06aded029ce15417bfd0d07e628f4623b
Joey Sokolon <SokoJoey@Gmail.com>
2018-03-31 18:18:48 -0400

    Added example axios request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/bye', (req, res) => res.send('Bye World!'))
@@ -7,4 +8,19 @@ app.get('/firstcommit', (req, res) => res.send('This is my first commit'))
 
 app.get('/helo', (req, res) => res.send('this is killing me.... help me....'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var servercode = () => {
+  console.log('Server started on port 3000!')
+  axios.get('https://hackathon.sipseller.net/central/rest/me', {
+      headers: {
+        'Authorization': "Basic dGVhbTFAbWFydGVsbG90ZWNoLmNvbTpwaW5lYXBwbGU="
+      }
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+app.listen(3000, servercode)
