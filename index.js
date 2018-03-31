1579ace8c3de8979b788672834633cae1342e8dd
Joey Sokolon <SokoJoey@Gmail.com>
2018-03-31 19:53:04 -0400

    Setup server to check for new events.

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/bye', (req, res) => res.send('Bye World!'))
-app.get('/firstcommit', (req, res) => res.send('This is my first commit'))
-
-app.get('/helo', (req, res) => res.send('this is killing me.... help me....'))
+var alerts = {};
 
 var servercode = () => {
   console.log('Server started on port 3000!')
@@ -16,7 +12,31 @@ var servercode = () => {
       }
     })
     .then(function(response) {
-      console.log(response.data);
+      Object.keys(response.data).map(key => alerts[key]=response.data[key])
+      setInterval(checkAlerts, 5000);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+var checkAlerts = () => {
+  axios.get('https://hackathon.sipseller.net/central/rest/devices/7aa4fb26-5a53-4677-a575-8623e87ba76b/alarmList/', {
+      headers: {
+        'Authorization': "Basic dGVhbTFAbWFydGVsbG90ZWNoLmNvbTpwaW5lYXBwbGU="
+      }
+    })
+    .then(function(response) {
+      var NewEvent = false
+      for (var key in response.data){
+        if (alerts[key] == undefined){
+          console.log("New alarm discovered!" + response.data[key])
+          NewEvent = true
+        }
+      }
+      if (NewEvent == false){
+        console.log("No New Alerts. Everything is working well!");
+      }
     })
     .catch(function(error) {
       console.log(error);
