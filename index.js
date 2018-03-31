b2fcc150f2b9ad30426e9877b85691be6a2071f3
Joey Sokolon <SokoJoey@Gmail.com>
2018-03-31 19:08:50 -0400

    Updated get to log alerts

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.get('/helo', (req, res) => res.send('this is killing me.... help me....'))
 
 var servercode = () => {
   console.log('Server started on port 3000!')
-  axios.get('https://hackathon.sipseller.net/central/rest/me', {
+  axios.get('https://hackathon.sipseller.net/central/rest/devices/7aa4fb26-5a53-4677-a575-8623e87ba76b/alarmList/', {
       headers: {
         'Authorization': "Basic dGVhbTFAbWFydGVsbG90ZWNoLmNvbTpwaW5lYXBwbGU="
       }
     })
     .then(function(response) {
-      console.log(response);
+      console.log(response.data);
     })
     .catch(function(error) {
       console.log(error);
