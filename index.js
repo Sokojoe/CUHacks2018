7a371554d8206ba05293cde9a6d79dcce427f84d
Helen Kim <jeehoneykim@gmail.com>
2018-03-31 14:47:30 -0400

    helen's first commit

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/bye', (req, res) => res.send('Bye World!'))
+app.get('/firstcommit', (req, res) => res.send('This is my first commit'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
