f6ab8aa63c368ac4c7d77fc097b6f41e6235c7b4
sewon <sewon.30@gmail.com>
2018-03-31 14:42:47 -0400

    Change

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/helo', (req, res) => res.send('this is killing me.... help me....'))
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
