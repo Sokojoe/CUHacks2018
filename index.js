32ee3eb65b5b05fa1fee45ee96dd331cff9e44d1
sewon <sewon.30@gmail.com>
2018-03-31 14:52:13 -0400

    Testing

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/bye', (req, res) => res.send('Bye World!'))
+app.get('/firstcommit', (req, res) => res.send('This is my first commit'))
 
 app.get('/helo', (req, res) => res.send('this is killing me.... help me....'))
 
