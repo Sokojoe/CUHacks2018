7055636be634a1e9fae5fd094a9acff133587ac6
Wesley Ma <wesleykhma@gmail.com>
2018-03-31 22:25:42 -0400

    Fixed sms reply

---
+++ API/CUHacks3L1D/amsHandler/functions/messaging/accept.js
@@ -0,0 +1,21 @@
+const lib = require('lib')({ token: process.env.STDLIB_TOKEN })
+const send = require('../../helpers/send.js')
+
+/**
+* MORE handler, responds if user texts "more"
+*  (or any uppercase variation like "MORE")
+* @param {string} sender The phone number that sent the text to be handled
+* @param {string} receiver The StdLib phone number that received the SMS
+* @param {string} message The contents of the SMS
+* @param {string} createdDatetime Datetime when the SMS was sent
+* @returns {any}
+*/
+module.exports = async (sender = '', receiver = '', message = '', createdDatetime = '', context) => {
+  // Callback to get active request info
+  // Callack to post response to node server
+  return send(
+    receiver,
+    sender,
+    "You have accepted the task."
+  )
+}
