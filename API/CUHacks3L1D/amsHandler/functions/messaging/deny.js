f2b74ff76cc1a22e82dfc4d53be410340ab0655d
Wesley Ma <wesleykhma@gmail.com>
2018-04-01 04:36:26 -0400

    Updated to use correct heroku links

--- API/CUHacks3L1D/amsHandler/functions/messaging/deny.js
+++ API/CUHacks3L1D/amsHandler/functions/messaging/deny.js
@@ -12,7 +12,7 @@ const axios = require('axios')
 */
 module.exports = async (sender = '', receiver = '', message = '', createdDatetime = '', context) => {
   // Callback to get active request info
-  let res = await axios.post('http://localhost:3000/deniedAlert', {"num": sender});
+  let res = await axios.post('https://fathomless-retreat-64446.herokuapp.com/deniedAlert', {"num": sender});
   console.log(res.data);
   return send(receiver, sender, "You (" + sender + ") have denied the task.")
 }
--- index.js
+++ index.js
@@ -2,78 +2,79 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 app.use(express.json());
-const lib = require('lib')({
-  token: "FpBaXmJxDDHDcfHKlHPKQQVFr29ccs3JtIJh8yOKlp2wNWRsEN3s6MCN-9XqP8SY"
-});
+const lib = require('lib')({token: "FpBaXmJxDDHDcfHKlHPKQQVFr29ccs3JtIJh8yOKlp2wNWRsEN3s6MCN-9XqP8SY"});
 const tel = lib.messagebird.tel['@0.0.21'];
 
 const originalPhonenum = "12048170807"
 
-var sysAdmins = {
-  "17058082706":"idk"
-}
+var sysAdmins = [] // Array of contacts
+sysAdmins.push({number: "17058082706", name: "idk"});
 
-var pendingAlarms = {
-  "12312":{
-    accepted:false,
-    assigned: null,
-  }
-}
+var pendingAlarms = []; // Array of pendingAlarms
+pendingAlarms.push({key: "12312", accepted: false, assigned: null}});
 
 var alerts = {};
-app.post('/acceptedAlert', function (req, res) {
-  console.log(req.body.num + " has accepted the request.");
-  res.send('Server accepted the request from ' + req.body.num);
-})
 
-app.post('/deniedAlert', function (req, res) {
-  console.log(req.body.num + " has denied the request.");
+app.post('/acceptedAlert', function(req, res) {
+if (pendingAlarms.length > 0) {
+  // Assign a user to an alarm
+  console.log('Server accepted the request from ' + req.body.num);
   res.send('Server accepted the request from ' + req.body.num);
-})
+} else {
+  console.log('No pendingAlarms');
+  res.send('No pendingAlarms');
+}
+});
+
+app.post('/deniedAlert', function(req, res) {
+if (pendingAlarms.length > 0) {
+  console.log('Server denied the request from ' + req.body.num);
+  res.send('Server denied the request from ' + req.body.num);
+} else {
+  console.log('No pendingAlarms');
+  res.send('No pendingAlarms');
+}
+});
 
 var servercode = () => {
-  console.log('Server started on port 3000!')
-  axios.get('https://hackathon.sipseller.net/central/rest/devices/7aa4fb26-5a53-4677-a575-8623e87ba76b/alarmList/', {
-      headers: {
-        'Authorization': "Basic dGVhbTFAbWFydGVsbG90ZWNoLmNvbTpwaW5lYXBwbGU="
-      }
-    })
-    .then(function(response) {
-      Object.keys(response.data).map(key => alerts[key] = response.data[key])
-      setInterval(checkAlerts, 10000);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+console.log('Server started on port 3000!')
+axios.get('https://hackathon.sipseller.net/central/rest/devices/7aa4fb26-5a53-4677-a575-8623e87ba76b/alarmList/', {
+  headers: {
+    'Authorization': "Basic dGVhbTFAbWFydGVsbG90ZWNoLmNvbTpwaW5lYXBwbGU="
+  }
+}).then(function(response) {
+  Object.keys(response.data).map(key => alerts[key] = response.data[key])
+  setInterval(checkAlerts, 10000);
+}).catch(function(error) {
+  console.log(error);
+});
 }
 
 var checkAlerts = () => {
-  axios.get('https://hackathon.sipseller.net/central/rest/devices/7aa4fb26-5a53-4677-a575-8623e87ba76b/alarmList/', {
-      headers: {
-        'Authorization': "Basic dGVhbTFAbWFydGVsbG90ZWNoLmNvbTpwaW5lYXBwbGU="
-      }
-    })
-    .then(function(response) {
-      for (var key in response.data) {
-        if (alerts[key] == undefined) {
-          console.log("New alarm discovered!" + response.data[key])
-          handleNewAlert(key);
-        }
-      }
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+axios.get('https://hackathon.sipseller.net/central/rest/devices/7aa4fb26-5a53-4677-a575-8623e87ba76b/alarmList/', {
+  headers: {
+    'Authorization': "Basic dGVhbTFAbWFydGVsbG90ZWNoLmNvbTpwaW5lYXBwbGU="
+  }
+}).then(function(response) {
+  for (var key in response.data) {
+    if (alerts[key] == undefined) {
+      console.log("New alarm discovered!" + response.data[key])
+      handleNewAlert(key);
+    }
+  }
+}).catch(function(error) {
+  console.log(error);
+});
 }
 
 function handleNewAlert(pendingAlarm, userPhone) {
-  tel.sms({
-    originator: originalPhonenum,
-    recipient: userPhone,
-    body: "If we ever see this text then our project is making really good progress"
-  }).catch((err) => {
-    console.log(err);
-  });
+tel.sms({
+  originator: originalPhonenum,
+  recipient: userPhone,
+  body: "ALERT: " + pendingAlarm.ke.
+}).catch((err) => {
+  console.log(err);
+});
 }
 
 handleNewAlert("", "17058082706")
